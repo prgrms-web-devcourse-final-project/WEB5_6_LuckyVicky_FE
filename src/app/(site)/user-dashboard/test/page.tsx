000357bdb60655616c6af3867b29d3284b8ad499
heroson02 <heroson0925@gmail.com>
2025-10-02 14:50:25 +0900

    feat : AuthorRequestModal

--- src/app/(site)/user-dashboard/test/page.tsx
+++ src/app/(site)/user-dashboard/test/page.tsx
@@ -1,6 +1,12 @@
+import AuthorRequestModal from '@/components/user-dashboard/AuthorRequestModal';
 import ExchangeRefundModal from '@/components/user-dashboard/ExchangeRefundModal';
 
 function page() {
-  return <ExchangeRefundModal />;
+  return (
+    <div>
+      <ExchangeRefundModal />
+      <AuthorRequestModal />
+    </div>
+  );
 }
 export default page;
