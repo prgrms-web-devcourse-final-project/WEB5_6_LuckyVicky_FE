e18f30208d181aadb954fbf1e9b9aa73b90621be
heroson02 <heroson0925@gmail.com>
2025-10-05 00:01:28 +0900

    fix : build error

--- src/app/(site)/search/page.tsx
+++ src/app/(site)/search/page.tsx
@@ -4,17 +4,13 @@ import { useMemo, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import ResultHeader from '@/components/search/ResultHeader';
 import ProductCard from '@/components/ProductCard';
-import { categoryData, Product } from '@/utils/categoryData';
 
-import Error from "@/assets/icon/error.svg";
+import Error from '@/assets/icon/error.svg';
 import { fuzzyMatch } from '@/utils/search';
+import { allProducts } from '@/utils/product';
 
 const SORTS = ['인기순', '최신순', '낮은 가격순', '높은 가격순'] as const;
-type Sort = typeof SORTS[number];
-
-export const allProducts:Product[] = Object.values(categoryData).flatMap((category) => category.products);
-
-
+type Sort = (typeof SORTS)[number];
 
 function toNumberPrice(v: string | number) {
   if (typeof v === 'number') return v;
@@ -30,23 +26,31 @@ export default function Page() {
   const filtered = useMemo(() => {
     // 검색어: (대소문자 무시)
     const keyword = q.toLowerCase();
-    
+
     const list = allProducts.filter(
       (item) =>
         !keyword ||
         fuzzyMatch(keyword, item.title) ||
-        fuzzyMatch(keyword, item.brand)
+        fuzzyMatch(keyword, item.brand),
     );
 
     // 정렬: FilteredSection과 동일 로직
     const copy = [...list];
     switch (sort) {
       case '최신순':
-        return copy.sort((a, b) => new Date(b.createdAt ?? 0).getTime() - new Date(a.createdAt ?? 0).getTime());
+        return copy.sort(
+          (a, b) =>
+            new Date(b.createdAt ?? 0).getTime() -
+            new Date(a.createdAt ?? 0).getTime(),
+        );
       case '낮은 가격순':
-        return copy.sort((a, b) => toNumberPrice(a.price) - toNumberPrice(b.price));
+        return copy.sort(
+          (a, b) => toNumberPrice(a.price) - toNumberPrice(b.price),
+        );
       case '높은 가격순':
-        return copy.sort((a, b) => toNumberPrice(b.price) - toNumberPrice(a.price));
+        return copy.sort(
+          (a, b) => toNumberPrice(b.price) - toNumberPrice(a.price),
+        );
       case '인기순':
       default:
         return copy;
@@ -63,9 +67,9 @@ export default function Page() {
         />
 
         {filtered.length === 0 ? (
-          <div className='flex flex-col justify-center items-center text-center p-50 mt-4 bg-tertiary-20'>
-            <Error width={90} height={90}  />
-            <span className='mt-4 mb-1'>검색결과가 없습니다.</span>
+          <div className="flex flex-col justify-center items-center text-center p-50 mt-4 bg-tertiary-20">
+            <Error width={90} height={90} />
+            <span className="mt-4 mb-1">검색결과가 없습니다.</span>
             <span>검색어를 다시 입력해주세요.</span>
           </div>
         ) : (
